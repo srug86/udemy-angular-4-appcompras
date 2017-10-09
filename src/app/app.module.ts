9ec7834c007b7e8089d5776fb0eff1ececcd4326
Sergio de la Rubia <srug86@gmail.com>
2017-10-10 00:40:24 +0200

    Añade el navegador de la aplicación.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { ProvidersService } from './services/providers.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProvidersComponent } from './providers/providers/providers.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'providers', component: ProvidersComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProvidersComponent
+    ProvidersComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ProvidersService],
   bootstrap: [AppComponent]
